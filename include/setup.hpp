f14a20aaa38c99902fc66fa56571f40fd3edb037
iungomadre <74658185+iungomadre@users.noreply.github.com>
2021-10-18 23:33:03 +0200

    Update setup.hpp

--- include/setup.hpp
+++ include/setup.hpp
@@ -3,13 +3,11 @@
 #if !defined(_SETUP_H)
 #define _SETUP_H
 
-#include <string>
-
 #define CONNECTION_TIMEOUT 30     // maksymalna ilość prób połączenia z siecią WiFi
 #define CONNECTION_TIMEOUT_ERROR    -1
 
 
 int connectWiFi(char* wifi_ssid, char* wifi_password, ESP8266WiFiClass& wifi_module);
-// connects to wifi network. throws exception when unavailable
+// connects to wifi network. returns CONNECTION_TIMEOUT_ERROR when unavailable
 
 #endif // _SETUP_H
